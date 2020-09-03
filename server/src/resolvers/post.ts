cc2fea215e7e6b0214c40f57a8c558285d87ed77
Alan Campora <alan.campora@gmail.com>
2020-09-03 00:59:35 -0300

    feat(server): finish post crud

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Ctx } from "type-graphql";
+import { Resolver, Query, Ctx, Mutation, Arg } from "type-graphql";
 import { Post } from "../entities/post";
 import { MyContext } from "../types";
 
@@ -7,5 +7,47 @@ export default class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() ctx: MyContext): Promise<Post[]> {
     return ctx.em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(@Arg("id") id: number, @Ctx() ctx: MyContext): Promise<Post | null> {
+    return ctx.em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title") title: string,
+    @Ctx() ctx: MyContext
+  ): Promise<Post> {
+    const post = ctx.em.create(Post, { title });
+    await ctx.em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id") id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Ctx() ctx: MyContext
+  ): Promise<Post | null> {
+    const post = await ctx.em.findOne(Post, { id });
+
+    if (!post) return null;
+
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await ctx.em.persistAndFlush(post);
+    }
+
+    return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id") id: number,
+    @Ctx() ctx: MyContext
+  ): Promise<Boolean> {
+    await ctx.em.nativeDelete(Post, { id });
+    return true;
   }
 }
