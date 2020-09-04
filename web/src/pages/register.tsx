fea12adc87dd029388e8d95be18059e48c5cab9b
Alan Campora <alan.campora@gmail.com>
2020-09-04 20:44:55 -0300

    feat(web): add register page

---
+++ web/src/pages/register.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Box,
+  Button,
+} from "@chakra-ui/core";
+import { Wrapper } from "../components/wrapper";
+import { InputField } from "../components/input";
+
+interface registerProps {
+  name: string;
+}
+
+const Register: React.FC<registerProps> = ({ hola }) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              variantColor="teal"
+            >
+              register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
