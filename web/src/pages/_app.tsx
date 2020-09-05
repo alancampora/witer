73a9efe44c5a3b4986ddd47d91e4a9ade34443f4
Alan Campora <alan.campora@gmail.com>
2020-09-05 02:40:41 -0300

    feat(web): update cache after user mutations

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,9 +1,65 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/core";
-import { createClient, Provider } from "urql";
+import { createClient, dedupExchange, fetchExchange, Provider } from "urql";
+import { cacheExchange, QueryInput, Cache } from "@urql/exchange-graphcache";
 import theme from "../theme";
+import { LoginMutation, MeDocument, MeQuery, RegisterMutation } from "../generated/graphql";
+
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: "http://localhost:4000/graphql",
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, _, cache, __) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                }
+                return {
+                  me: {
+                    ...result.login.user
+                  }
+                }
+              }
+            );
+          },
+
+          register: (_result, _, cache, __) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                }
+                return {
+                  me: {
+                    ...result.register.user
+                  }
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
   fetchOptions: {
     credentials: "include", //seding the cookie
   },
